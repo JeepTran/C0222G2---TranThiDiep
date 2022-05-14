9ffe620044ba3f791b5dcabe1e86119138ab982a
C0222G2-TranThiDiep <chenshiye1005@gmail.com>
2022-05-14 23:22:28 +0700

    Database 14/05/2022

--- module3/ss1_relational_database/bai_tap/bai_tap_1_quan_ly_sinh_vien.sql
+++ module3/ss1_relational_database/bai_tap/bai_tap_1_quan_ly_sinh_vien.sql
@@ -1,18 +1,19 @@
+drop database if exists student_management;
 create database student_management;
 use student_management;
 
 create table student(
-id int,
+student_id int primary key,
 student_name varchar(45),
 age int,
-country varchar(255));
+country varchar(45));
 
 create table class(
-id int,
-class_name varchar(255));
+class_id int primary key,
+class_name varchar(45));
 
 create table teacher(
-id int,
+teacher_id int primary key,
 teacher_name varchar(45),
 age int,
 country varchar(45));
