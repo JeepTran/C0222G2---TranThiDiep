6db389aef67f69533528c7f7258d456621d3a18d
C0222G2-TranThiDiep <chenshiye1005@gmail.com>
2022-06-25 15:53:50 +0700

    Spring Validate 25/06/2022

--- module4/ss8_validation/bai_tap/bai_tap_2_validate_thong_tin_bai_hat/src/main/java/com/jeep/bai_tap_2_validate_thong_tin_bai_hat/controller/SongController.java
+++ module4/ss8_validation/bai_tap/bai_tap_2_validate_thong_tin_bai_hat/src/main/java/com/jeep/bai_tap_2_validate_thong_tin_bai_hat/controller/SongController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -30,6 +31,12 @@ public class SongController {
         return modelAndView;
     }
     @GetMapping("/create")
+    public String displayCreateForm(Model model){
+        model.addAttribute("songDto", new SongDto());
+        return "create-song";
+    }
+
+    @PostMapping("/create")
     public String createNewSong(@ModelAttribute @Valid SongDto songDto,
                                 BindingResult bindingResult, RedirectAttributes redirectAttributes){
 
