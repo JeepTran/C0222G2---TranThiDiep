06d55a9fa20b90f4f49859caf9881c6a95874a13
C0222G2-TranThiDiep <chenshiye1005@gmail.com>
2022-07-19 23:12:28 +0700

    Casestudy 5

---
+++ module5/casestudy5-angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FacilityListComponent } from './facility/facility-list/facility-list.component';
+import { FacilityCreateComponent } from './facility/facility-create/facility-create.component';
+import { FacilityEditComponent } from './facility/facility-edit/facility-edit.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { ContractListComponent } from './contract/contract-list/contract-list.component';
+import { ContractCreateComponent } from './contract/contract-create/contract-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FacilityListComponent,
+    FacilityCreateComponent,
+    FacilityEditComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    CustomerEditComponent,
+    ContractListComponent,
+    ContractCreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
