a78c61ba44928034542d445a764d2204159935d3
C0222G2-TranThiDiep <chenshiye1005@gmail.com>
2022-09-06 14:03:14 +0700

    diep update

---
+++ module6_sprint2/Project/demo-jwt/angular7-jwt/src/app/service/authentication.service.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+export class User {
+  constructor(public status: string) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  authenticate(username, password) {
+    let user = {
+      username: username,
+      password: password
+    }
+    const headers = new HttpHeaders({Authorization: 'Basic ' + btoa(username + ':' + password)});
+    return this.httpClient.post<User>('http://localhost:8080/authenticate',user, {headers}).pipe(
+      map(
+        userData => {
+          sessionStorage.setItem('username', username);
+          return userData;
+        }
+      )
+    );
+  }
+
+
+  isUserLoggedIn() {
+    const user = sessionStorage.getItem('username');
+    // console.log(!(user === null));
+    return !(user === null);
+  }
+
+  logOut() {
+    sessionStorage.removeItem('username');
+  }
+}
